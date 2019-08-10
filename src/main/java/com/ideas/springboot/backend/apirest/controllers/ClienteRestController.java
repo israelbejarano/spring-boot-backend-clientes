72221002bd2453b298d3d62596d7257225721e22
israel <israel.bejarano.a@gmail.com>
2019-08-10 11:54:13 +0200

    habilitado el CORS para Angular

--- src/main/java/com/ideas/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/ideas/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -3,6 +3,7 @@ package com.ideas.springboot.backend.apirest.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import com.ideas.springboot.backend.apirest.models.services.IClienteService;
  * The Class ClienteRestController.
  * @author Israel Bejarano
  */
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
